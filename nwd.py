d27d65b2dc063bbd009f146de2cb0e109e3737b9
Jakub Jablonski <jakub.jablonski@data.pl>
2024-01-18 20:39:04 +0100

    a

--- nwd.py
+++ nwd.py
@@ -1,12 +1,29 @@
 
-def nwd_i(a, b):
-    while b:
-        a, b = b, a%b
+def nwd(a, b):
+    while a != b:
+        if a > b:
+            print(f'{a} > {b}')
+            a = a - b
+        else:
+            print(f'{b} > {a}')
+            b = b - a
     return a
 
-a = int(input("Podaj a:"))
-b = int(input("Podaj b:"))
+def nww(a, b):
+    return a * b / nwd_j(a, b)
 
-nwd = nwd_i(a, b)
+a = int(input("Podaj a:"))
+if a <= 0:
+    print('Liczba a musi być dodatnia')
+    quit()
+
+b = int(input("Podaj b:"))
+if b <= 0:
+    print('Liczba b musi być dodatnia')
+    quit()
+
+nwd = nwd(a, b)
+nww = nww(a, b)
 
 print(f'nwd({a}, {b}) = {nwd}')
+print(f'nww({a}, {b}) = {nww}')
